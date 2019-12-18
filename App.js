77bcc5cab8608252ac7ebf846ac7856e9bd07eb5
Sujaany <ssujaany09@gmail.com>
2019-12-18 14:43:08 +0530

    Palmyrah folder modified

--- App.js
+++ App.js
@@ -1,12 +1,12 @@
 import React, {Component} from 'react';
 import {StyleSheet, View} from 'react-native';
 
-import MapIntegration from './components/mapintegration';
+// import MapIntegration from './components/mapintegration';
 import Login from './components/Login';
 // import CurrentLocationMap from './components/map/map';
 // import ImagePalmyrah from './components/Palmyrah/Image/imagePalmyrah';
 // import ImageVayuresort from './components/VayuResort/Image/imageVayuresort';
-import SoundPalmyrah from './components/Palmyrah/audio';
+// import SoundPalmyrah from './components/Palmyrah/audio';
 
 //this is main
 export default class App extends Component {
--- components/Palmyrah/Image/imagePalmyrah.js
+++ components/Palmyrah/Image/imagePalmyrah.js
@@ -1,35 +1,22 @@
 import React, { Component } from "react";
 import {
     View,
-<<<<<<< HEAD
-    StyleSheet,
-    SafeAreaView,
-    Platform,
-=======
     Text,
     StyleSheet,
     SafeAreaView,
     TextInput,
     Platform,
     StatusBar,
->>>>>>> f275aae06997e97b28d56b5ca31a7395d088b77f
     ScrollView,
     Image,
     Dimensions
 } from "react-native";
-<<<<<<< HEAD
-import Category from '../Palmyrah/Category'
-=======
 import Category from './category'
->>>>>>> f275aae06997e97b28d56b5ca31a7395d088b77f
 
 const { height, width } = Dimensions.get('window')
 
 export default class ImagePalmyrah extends Component {
-<<<<<<< HEAD
-=======
 
->>>>>>> f275aae06997e97b28d56b5ca31a7395d088b77f
     render() {
         return (
             <SafeAreaView style={{ flex: 1 }}>
@@ -43,12 +30,8 @@ export default class ImagePalmyrah extends Component {
                             shadowOpacity: 0.2,
                             elevation: 1,
                             marginTop: Platform.OS == 'android' ? 30 : null
-<<<<<<< HEAD
-                        }}>    
-=======
                         }}>
                             
->>>>>>> f275aae06997e97b28d56b5ca31a7395d088b77f
                         </View>
                     </View>
                     <ScrollView
@@ -59,36 +42,41 @@ export default class ImagePalmyrah extends Component {
                                 <View style={{ width: width - 40, height: 200, marginTop: 20 }}>
                                     <Image
                                         style={{ flex: 1, height: null, width: null, resizeMode: 'cover', borderRadius: 5, borderWidth: 1, borderColor: '#dddddd' }}
-                                        source={require('../../assets/Palmyrah/Image/1.jpg')}
+                                        source={require('../../../assets/Palmyrah/Image/1.jpg')}
                                     />
 
                                 </View>
                             </View>
                             
-                            <View style={{ flex:1 ,flexDirection:'row', height: 130, marginTop: 15}}>
+                            <View style={{ height: 130, marginTop: 15}}>
                                 <ScrollView
                                     horizontal={true}
                                     showsHorizontalScrollIndicator={false}
                                 >
-                                    <Category imageUri={require('../../assets/Palmyrah/Image/1.jpg')}
+                                    <Category imageUri={require('../../../assets/Palmyrah/Image/1.jpg')}
                                         name="Home"
                                     />
-                                    <Category imageUri={require('../../assets/Palmyrah/Image/2.jpg')}
+                                    <Category imageUri={require('../../../assets/Palmyrah/Image/2.jpg')}
                                         name="Experiences"
                                     />
-                                    <Category imageUri={require('../../assets/Palmyrah/Image/3.jpg')}
+                                    <Category imageUri={require('../../../assets/Palmyrah/Image/3.jpg')}
                                         name="Resturant"
                                     />
-                                    <Category imageUri={require('../../assets/Palmyrah/Image/4.jpg')}
+                                    <Category imageUri={require('../../../assets/Palmyrah/Image/4.jpg')}
                                         name="Home"
                                     />
-                                    <Category imageUri={require('../../assets/Palmyrah/Image/5.jpg')}
+                                    <Category imageUri={require('../../../assets/Palmyrah/Image/5.jpg')}
                                         name="Experiences"
                                     />
+                                    <Category imageUri={require('../../../assets/Palmyrah/Image/6.jpg')}
+                                        name="Resturant"
+                                    />
                                 </ScrollView>
-                            </View>                    
+                            </View>
+                           
                         </View>
                     </ScrollView>
+
                 </View>
             </SafeAreaView>
         );
@@ -98,7 +86,7 @@ export default class ImagePalmyrah extends Component {
 const styles = StyleSheet.create({
     container: {
         flex: 1,
-        alignItems: 'stretch',
+        alignItems: 'center',
         justifyContent: 'center'
     }
 });
